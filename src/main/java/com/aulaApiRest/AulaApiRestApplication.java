42fa3737a52ad0b3521dc28992c44a62d8732522
Fernanda <feernandaoliveira01@gmail.com>
2022-08-30 10:18:07 -0300

    Project created

---
+++ src/main/java/com/aulaApiRest/AulaApiRestApplication.java
@@ -0,0 +1,13 @@
+package com.aulaApiRest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AulaApiRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AulaApiRestApplication.class, args);
+	}
+
+}
